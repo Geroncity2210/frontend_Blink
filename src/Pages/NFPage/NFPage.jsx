392b471a198b50c22aec6abd542c959490e98b68
Geroncity2210 <juanpablomorenopatarroyo1@gmail.com>
2024-05-26 21:18:04 -0500

    err404Page

---
+++ src/Pages/NFPage/NFPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NFPage.css";
+export const NFPage = () => {
+  return (
+    <>
+      <body>
+        <div className="err">
+          <h2>Blinke</h2>
+          <div className="error_message">
+            <h1>404</h1>
+            <h2>Error</h2>
+            <p>Ehh...Where u goin'?</p>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+};
