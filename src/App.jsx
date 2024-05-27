8814a4052672c647e6048d77e0bd55034227bc0c
Geroncity2210 <juanpablomorenopatarroyo1@gmail.com>
2024-05-26 19:06:35 -0500

    404pagedone

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { MainPage, LoginPage, SignUpPage, Homepage, CreateBlink, Myprofile, SearchPage } from "./Pages";
-import AuthProvider, {AuthIsNotSignedIn,AuthIsSignedIn,} from "./context/context";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import {
+  MainPage,
+  LoginPage,
+  SignUpPage,
+  Homepage,
+  CreateBlink,
+  Myprofile,
+  SearchPage,
+} from "./Pages";
+import AuthProvider, {
+  AuthIsNotSignedIn,
+  AuthIsSignedIn,
+} from "./context/context";
+import { NFPage } from "./Pages/NFPage/NFPage";
 
 function App() {
   return (
     <AuthProvider>
-    <AuthIsSignedIn>
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/create" element={<CreateBlink />} />
-          <Route path="/profile" element={<Myprofile />} />
-          <Route path="/search" element={<SearchPage/>} />
-          <Route path="*" element={<Navigate replace to={"/404"} />} />
-        </Routes>
-      </Router>
-    </AuthIsSignedIn>
-    <AuthIsNotSignedIn>
-      <Router>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path="/signin" element={<SignUpPage />} />
-          <Route path={"/login"} element={<LoginPage />} />
-          <Route path="*" element={<Navigate replace to={"/"} />} />
-        </Routes>
-      </Router>
-    </AuthIsNotSignedIn>
+      <AuthIsSignedIn>
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/create" element={<CreateBlink />} />
+            <Route path="/profile" element={<Myprofile />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="*" element={<Navigate replace to={"/404"} />} />
+            <Route path="/404" element={<NFPage />} />
+          </Routes>
+        </Router>
+      </AuthIsSignedIn>
+      <AuthIsNotSignedIn>
+        <Router>
+          <Routes>
+            <Route path={"/"} element={<MainPage />} />
+            <Route path="/signin" element={<SignUpPage />} />
+            <Route path={"/login"} element={<LoginPage />} />
+            <Route path="*" element={<Navigate replace to={"/"} />} />
+            <Route path="/404" element={<NFPage />} />
+          </Routes>
+        </Router>
+      </AuthIsNotSignedIn>
     </AuthProvider>
   );
 }
